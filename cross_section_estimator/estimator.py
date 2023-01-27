9e4e93ed0e0770f250be9b10e307f6e179b16757
Luke Siemens <luke@lsiemens.com>
2023-01-26 17:56:18 -0800

    Add description for cross section estimator

--- cross_section_estimator/estimator.py
+++ cross_section_estimator/estimator.py
@@ -1,5 +1,35 @@
+"""
+estimator.py: Estimate collision cross sections
+
+Refrence "Introduction to Elementary Particles" by Griffiths, 2nd Ed
+Chapter 9 section 6: Neutral Weak Interactions
+
+In paticular compute the total cross section from the differential
+cross section for e^+ + e^- -> f + \\bar{f}. Following Eample 9.5
+"""
+
 from matplotlib import pyplot
 import numpy
+
+# constants values from Griffiths
+theta_w = 28.78 # in degrees, the weak mixing angle
+sin2_theta_w = 0.2314 # unitless, sin^2(theta_w)
+
+# todo select couplings from desired flaver
+# Neutral vector and axial vector couplings
+fermion_couplings = {"lepton":{""}, "quark":{}}
+
+
+c_V = 1/2
+c_A = 1/2
+m_f = 0
+
+#
+# dsigma_dOmega = (g_z^2E/[16pi([2E]^2 - [M_z]^2)])^2 *
+# {[(c_Vf)^2 + (c_Af)^2][(c_Ve)^2 + (c_Ae)^2](1 + cos^2(theta) - 8c_Vf c_Af c_Ve c_Ae cos(theta)}
+#
+
+##### break: old code #####################################
 
 #
 # toy dsigmadOmega = (E/(E^2 - M_z^2))^2*(A(1 + cos^2(theta)) - Bcos(theta))
